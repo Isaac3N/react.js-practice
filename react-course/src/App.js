b5cb86b7763b89485d4d26d417388332881eb3c7
Isaac3N <theisaac3n@gmail.com>
2021-03-07 01:57:18 +0100

    components basics

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>hi, <Person/></p>
+    <Message/>
+    <h2>this is a component</h2>
     </div>
+    
   );
 }
+
+const Person = () => <h2>Isaac</h2>
+const Message = () => {
+  return(
+    <h2>How are you doing today</h2>
+  )
+}
+
+
 
 export default App;
